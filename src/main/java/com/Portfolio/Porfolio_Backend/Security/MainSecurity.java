91e62dcd1302d31344b1ab281279aef51fb821ee
Colby <vertiluscolby@gmail.com>
2023-05-11 01:10:44 -0300

    Backend Portafolio web

--- src/main/java/com/Portfolio/Porfolio_Backend/Security/MainSecurity.java
+++ src/main/java/com/Portfolio/Porfolio_Backend/Security/MainSecurity.java
@@ -21,6 +21,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 
 @Configuration
@@ -56,6 +57,11 @@ public class MainSecurity {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
        return configuration.getAuthenticationManager();
+    }
+
+    @Bean(name = "mvcHandlerMappingIntrospector")
+    public HandlerMappingIntrospector mvcHandlerMappingIntrospector() {
+        return new HandlerMappingIntrospector();
     }
 
     @Bean
