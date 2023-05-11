2b02a2f6bf643358a0e26698df21b783bf788052
Colby <cvertilus@fi.uba.ar>
2023-05-11 10:37:25 -0300

    Backend portfolio web

--- src/main/java/com/Portfolio/Porfolio_Backend/Security/JsonWebToken/JWTProvider.java
+++ src/main/java/com/Portfolio/Porfolio_Backend/Security/JsonWebToken/JWTProvider.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -20,7 +21,7 @@ import java.util.function.Function;
 /**
  * @author Samson Effes
  */
-@Service
+@Component
 @AllArgsConstructor
 @NoArgsConstructor
 public class JWTProvider {
